7f6004e6a5bbf43662454b5d55e09be721c16f30
jason <liujpnz@gmail.com>
2018-07-09 05:12:44 +0000

    day1

--- git.sh
+++ git.sh
@@ -12,3 +12,16 @@ git commit -m "This is my first commit!"
 
 
 git push -u origin master
+
+
+#create a new branch and check you in new branch
+git checkout -b newbranch
+
+#confirm your branch
+git branch
+
+#push a branch to github
+git push origin newbranch
+
+
+
